94ba0e7de02bdb8a054a66e0805edeef919df4d3
IceScream-IL <dcrhtk98@gmail.com>
2025-11-13 23:20:02 +0200

    .

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,4 +1,26 @@
 
-public class Enemy {
+public class Enemy extends Character {
+	private int damage;
 
+	public Enemy(String name, int health, int level, int damage) {
+		super(name, health, level);
+		if (damage > 5) {
+			this.damage = 5;
+			System.out.println("Damage above 5 is impossible - Damage has been set to 5");
+		} else if (health < 1) {
+			this.damage = 1;
+			System.out.println("Damage below 1 is impossible - Damage has been set to 1");
+		} else {
+			this.damage = damage;
+		}
+	}
+
+	public int getDamage() {
+		return this.damage;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "\nDamage: " + this.damage;
+	}
 }
