7710471f0f4ae00e4e67c09daf5c01dc9f0a889a
IceScream-IL <dcrhtk98@gmail.com>
2025-11-13 23:04:18 +0200

    .

---
+++ src/Character.java
@@ -0,0 +1,67 @@
+
+public class Character {
+	private String name;
+	private int health;
+	private int level;
+
+	public Character() {
+		this.name = "Unknown";
+		this.health = 0;
+		this.level = 0;
+	}
+
+	public Character(String name, int health, int level) {
+		this.name = name;
+		if (health > 10) {
+			this.health = 10;
+			System.out.println("Health above 10 is impossible - Health has been set to 10");
+		} else if (health < 0) {
+			this.health = 0;
+			System.out.println("Health below 0 is impossible - Health has been set to 0");
+		} else {
+			this.health = health;
+		}
+
+		if (level > 5) {
+			this.level = 5;
+			System.out.println("Level above 5 is impossible - Level has been set to 5");
+		} else if (level < 0) {
+			this.level = 0;
+			System.out.println("Level below 0 is impossible - Level has been set to 0");
+		} else {
+			this.level = level;
+		}
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getHealth() {
+		return this.health;
+	}
+
+	public int getLevel() {
+		return this.level;
+	}
+
+	public void levelUp() {
+		if (this.level == 5) {
+			System.out.println("Max level reached - cant level up");
+		} else {
+			this.level++;
+		}
+	}
+
+	public void heal(int amount) {
+		if (this.health + amount >= 10) {
+			this.health = 10;
+		} else {
+			this.health += amount;
+		}
+	}
+
+	public String toString() {
+		return "Name: " + this.name + "\n" + "Health: " + this.health + "\n" + "Level: " + this.level;
+	}
+}
---
+++ src/Item.java
@@ -0,0 +1,43 @@
+
+public class Item {
+	private String name;
+	private int value;
+	private boolean isMagical;
+
+	public Item() {
+		this.name = "Unknown";
+		this.value = 0;
+		this.isMagical = false;
+	}
+
+	public Item(String name, int value, boolean isMagical) {
+		this.name = name;
+		this.value = value;
+		this.isMagical = isMagical;
+	}
+
+	public String toString() {
+		String magicalStatus = isMagical ? "Magical item" : "Regular item";
+		return "Name: " + this.name + "\n" + "Value: " + this.value + "\n" + magicalStatus;
+	}
+
+	public int getValue() {
+		return this.value;
+	}
+
+	public void setValue(int value) {
+		if (value < 0) {
+			this.value = 0;
+		} else {
+			this.value = value;
+		}
+	}
+
+	public boolean isMagical() {
+		return this.isMagical;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+}
